23a5cf4974c64df43ea6f17c2cc111a9cb9f8c8e
Aaron-Penguin <202205433@post.au.dk>
2026-02-12 18:05:18 +0100

    Chainged folder names

--- 3. Basics of C++ programming/Exercise_2 and 3/main.cc
+++ 3. Basics of C++ programming/Exercise_2 and 3/main.cc
@@ -6,5 +6,10 @@ int main() {
     for (double i = 1; i < 11; i++) {
         double gamma_func = sfuns::fgamma(i);
         long double gamma_stir = sfuns::stirling_gamma(i);
+
         std::cout << "Gamma("<< i << ") =" << gamma_func << "   Gamma_stir("<< i <<") =" << gamma_stir << "\n";}
+    for (double i = 11; i < 21; i++) {
+        double gamma_func = sfuns::fgamma(i);
+        std::cout << "Gamma("<< i << ") =" << gamma_func << "\n";
+    }
 }
--- 3. Basics of C++ programming/Exercise_2 and 3/source.cc
+++ 3. Basics of C++ programming/Exercise_2 and 3/source.cc
@@ -9,10 +9,10 @@ constexpr double e = std::numbers::e;
 
 // constexpr double PI = 3.14159265358979323846; // pre- c++20
 double fgamma(double x){
-	if(x<0)return PI/std::sin(PI*x)/fgamma(1-x);
-	if(x<9)return fgamma(x+1)/x;
-	double lnfgamma=x*std::log(x+1/(12*x-1/x/10))-x+std::log(2*PI/x)/2;
-	return std::exp(lnfgamma);
+	if(x<0) {return PI/std::sin(PI*x)/fgamma(1-x);}
+	if(x<9) {return fgamma(x+1)/x;}
+	else {double lnfgamma=x*std::log(x+1/(12*x-1/x/10))-x+std::log(2*PI/x)/2;
+	return std::exp(lnfgamma);}
 	}
 
 // double stirling_gamma(double x){return std::pow(x,x) * std::pow(e,-x);}
