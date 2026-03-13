9510f3c6c876cf926f2d716ec5a73eada5c3d142
Aaron-Penguin <202205433@post.au.dk>
2026-03-13 21:58:31 +0100

    stuff

--- 9.Linear_eq/Exercise_C/main.cc
+++ 9.Linear_eq/Exercise_C/main.cc
@@ -1,24 +1,30 @@
 #include<iostream>
-#include<vector>
+#include<cstdlib>
+
 #include "Matrix.cc"
 #include "QR.cc"
-
-
 using namespace std;
 
 
 int main() {
 
+int N = 3;
+double A[N][N];
 
-double A[3][2] = {
-    {1,2},
-    {1,0},
-    {1,0}
-};
+for (int i =0; i < N; ++i){
+    for (int j=0; j < N; ++j){
+        A[i][j] =  (rand() % 101);
+    }
+}
 
+
+// double B[2][2] = {
+//     {1,2},
+//     {4,0}
+// };
 
 Matrix mat_A(A);
-QR qr(mat_A);
+mat_A.print();
 
 
 return 0; 
